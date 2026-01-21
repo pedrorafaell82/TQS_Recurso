d8642fec01197012fce794cd09d54391693429ce
pedrorafaell82 <pedrorlr@ua.pt>
2026-01-21 18:41:36 +0000

    SpringBoot Setup

---
+++ backend/tqs/src/main/java/pedro/tqs/TqsApplication.java
@@ -0,0 +1,13 @@
+package pedro.tqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqsApplication.class, args);
+	}
+
+}
---
+++ backend/tqs/src/test/java/pedro/tqs/TqsApplicationTests.java
@@ -0,0 +1,13 @@
+package pedro.tqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
